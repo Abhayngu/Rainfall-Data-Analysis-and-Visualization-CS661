53acd6881ebb6fc5b0e1fa39d3b3900ef8291890
Abhayngu <masterdwivedi@gmail.com>
2023-04-20 14:53:15 +0530

    changes done

--- temp_pressure_correlation.py
+++ temp_pressure_correlation.py
@@ -5,8 +5,8 @@ import plotly.express as px
 from urllib.request import urlopen
 import geopandas as gpd 
 
-pressureData = pd.read_csv("india_monthly_rainfall_data.csv")
-temperatureData = pd.read_csv("temp.csv")
+rainfallData = pd.read_csv("india_monthly_rainfall_data.csv")
+temperatureData = pd.read_csv("temperatures.csv")
 # print(temperatureData)
 
 pd.set_option('display.max_rows', None)
@@ -14,9 +14,9 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-pressureData.loc[pressureData['State']=='Chattisgarh','State'] = "Chhattisgarh"
-pressureData.loc[pressureData['State']=='Delhi','District'] = "Delhi"
-pressureData.loc[pressureData['State']=='Jammu & Kashmir','State'] = 'Jammu and Kashmir'
+rainfallData.loc[rainfallData['State']=='Chattisgarh','State'] = "Chhattisgarh"
+rainfallData.loc[rainfallData['State']=='Delhi','District'] = "Delhi"
+rainfallData.loc[rainfallData['State']=='Jammu & Kashmir','State'] = 'Jammu and Kashmir'
 
 
 months={
@@ -34,14 +34,15 @@ months={
     12: 'Dec'
 }
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 
 
 
 app.layout = html.Div([
-        html.H1('Correlation between Temperature and Pressure in India', style={'color': 'Grey' , 'text-align': 'center' , 'margin-bottom': '10px'}),
         html.Div([
+        html.H1('Change in Average rainfall and average temperature', style={'color': 'Grey' , 'textAlign': 'center'}),
+        
         dcc.Slider(
             id='slider',
             min=1901,
@@ -55,28 +56,34 @@ app.layout = html.Div([
                 1980: '1980',
                 2003: '2003'
             },
-            value = 2000
+            tooltip={"placement": "bottom", "always_visible": True},
+            value = 2000,
+            className='year-slider'
         ),
-        dcc.Graph(id='temp-pressure-correlation', style={'backgroundColor' : 'red'})
+        html.Div(children = [ dcc.Graph(id='rainfall-correlation', style={'backgroundColor' : 'red'}),
+        dcc.Graph(id='temp-correlation', style={'backgroundColor' : 'red'}),], style={'display' : 'flex', 'justifyContent' : 'space-between'})
+       
+        # dcc.Graph(id='rainfall-temp-correlation', style={'backgroundColor' : 'red'})
     ]),
    
 ])
 
 
 
-@app.callback(Output('temp-pressure-correlation', 'figure'), Input('slider', 'value'))
-def updateCorrelationGraph(sliderVal):
-    temp = pressureData[pressureData['Year']==sliderVal]
+@app.callback(Output('rainfall-correlation', 'figure'), Input('slider', 'value'))
+def updateRainfall(sliderVal):
+    print('changing rainfall')
+    temp = rainfallData[rainfallData['Year']==sliderVal]
     # print(temp[0:5])
     dfR = pd.DataFrame({'avgRainfall' : []})
-    dfT = pd.DataFrame({'avgTemperature' : []})
+    # dfT = pd.DataFrame({'avgTemperature' : []})
     for m in months:
         t1 = temp[months[m]]
         avgRainfall = t1.sum()/len(t1)
-        t1 = temperatureData[months[m]]
-        avgTemperature = t1.sum()/len(t1)
         dfR.loc[m, 'avgRainfall'] = avgRainfall
-        dfT.loc[m, 'avgTemperature'] = avgTemperature
+        # t1 = temperatureData[months[m]]
+        # avgTemperature = t1.sum()/len(t1)
+        # dfT.loc[m, 'avgTemperature'] = avgTemperature
         # print('Average rainfall Overall india in ', months[m], ' : ', avgRainfall)
         # print('Average Temperature Overall india in ', months[m], ' : ', avgTemperature)
        
@@ -87,12 +94,66 @@ def updateCorrelationGraph(sliderVal):
         # print(t1[0:5])
     df = pd.DataFrame()
     df['avgRainfall'] = dfR
-    df['avgTemperature'] = dfT
-    print('----------------------------------------')
-    print(df)
-    # df = px.data.gapminder().query("continent=='Oceania'")
-    fig = px.line(df, x=list(months.values()), y=['avgRainfall', 'avgTemperature'])
+    # df['avgTemperature'] = dfT
+    fig = px.line(df, x=list(months.values()), y=['avgRainfall'])
     return fig
+
+@app.callback(Output('temp-correlation', 'figure'), Input('slider', 'value'))
+def updateTemperature(sliderVal):
+    print('changing temp')
+    temp = temperatureData[temperatureData['Year']==sliderVal]
+    # print(temp[0:5])
+    # dfR = pd.DataFrame({'avgRainfall' : []})
+    dfT = pd.DataFrame({'avgTemperature' : []})
+    for m in months:
+        # t1 = temp[months[m]]
+        # avgRainfall = t1.sum()/len(t1)
+        t1 = temp[months[m]]
+        avgTemperature = t1.sum()/len(t1)
+        # dfR.loc[m, 'avgRainfall'] = avgRainfall
+        dfT.loc[m, 'avgTemperature'] = avgTemperature
+        # print('Average rainfall Overall india in ', months[m], ' : ', avgRainfall)
+        # print('Average Temperature Overall india in ', months[m], ' : ', avgTemperature)
+       
+        # print(type(t1))
+        # print(months[m], t1[0:5], sep='\n')
+        # print('length ', len(t1))
+        # print('type ', type(t1))
+        # print(t1[0:5])
+    df = pd.DataFrame()
+    # df['avgRainfall'] = dfR
+    df['avgTemperature'] = dfT
+    fig = px.line(df, x=list(months.values()), y=['avgTemperature'], color_discrete_sequence=['red'])
+    return fig
+
+# @app.callback(Output('rainfall-temp-correlation', 'figure'), Input('slider', 'value'))
+# def updateCorrelation(sliderVal):
+#     temp = rainfallData[rainfallData['Year']==sliderVal]
+#     # print(temp[0:5])
+#     dfR = pd.DataFrame({'avgRainfall' : []})
+#     dfT = pd.DataFrame({'avgTemperature' : []})
+#     for m in months:
+#         t1 = temp[months[m]]
+#         avgRainfall = t1.sum()/len(t1)
+#         t1 = temperatureData[months[m]]
+#         avgTemperature = t1.sum()/len(t1)
+#         dfR.loc[m, 'avgRainfall'] = avgRainfall
+#         dfT.loc[m, 'avgTemperature'] = avgTemperature
+#         # print('Average rainfall Overall india in ', months[m], ' : ', avgRainfall)
+#         # print('Average Temperature Overall india in ', months[m], ' : ', avgTemperature)
+       
+#         # print(type(t1))
+#         # print(months[m], t1[0:5], sep='\n')
+#         # print('length ', len(t1))
+#         # print('type ', type(t1))
+#         # print(t1[0:5])
+#     df = pd.DataFrame()
+#     df['avgRainfall'] = dfR
+#     df['avgTemperature'] = dfT
+    
+#     fig = px.line(df, x='avgTemperature', y='avgRainfall', color_discrete_sequence=['red'])
+
+#     return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True,use_reloader=True, threaded=True)
